0f2fdf9dc199b8192fea74270d8438117e586655
Todd Halfpenny <todd@toddhalfpenny.com>
2023-02-24 15:38:28 +0000

    chore: blat test

--- test/commands/pkgvsn/monitor.test.ts
+++ test/commands/pkgvsn/monitor.test.ts
@@ -4,14 +4,7 @@ describe('pkgvsn monitor', () => {
   test
     .stdout()
     .command(['pkgvsn monitor'])
-    .it('runs hello', (ctx) => {
-      expect(ctx.stdout).to.contain('hello world');
-    });
-
-  test
-    .stdout()
-    .command(['pkgvsn monitor', '--name', 'Astro'])
-    .it('runs hello --name Astro', (ctx) => {
-      expect(ctx.stdout).to.contain('hello Astro');
+    .it('runs monitor', (ctx) => {
+      expect(ctx.stdout).to.contain('');
     });
 });
